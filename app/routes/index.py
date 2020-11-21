96c58451eb5945651b7b5db4c815e4f2855f9c05
Adam Verner <adam.verner@mensa.cz>
2020-11-21 16:37:33 +0100

    map generation

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,6 +1,5 @@
-from flask import render_template, flash
+from flask import render_template
 
-from app.data import teams, waypoints
 from app.routes import bp
 from app.models import Waypoint, Team, TeamFoundWaypoint
 
@@ -14,3 +13,10 @@ def index():
     print(Team.query.all())
     print(Waypoint.query.all())
     return render_template('index.html', teams=Team.query.all(), waypoints=Waypoint.query.all())
+
+
+@bp.route('/map.js')
+def map_gen():
+    waypoints = Waypoint.query.all()
+    print(waypoints)
+    return render_template('map_gen.js', waypoints=waypoints)
---
+++ app/templates/map_gen.js
@@ -0,0 +1,55 @@
+let layer;
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    Loader.async = true;
+    console.log('calling Loader');
+    Loader.load(null, null, function () {
+        console.log('Loading stuff');
+
+        const center = SMap.Coords.fromWGS84(14.41790, 50.12655);
+        const map = JAK.gel("map");
+        const m = new SMap(JAK.gel("map"), center, 9);
+        m.addDefaultLayer(SMap.DEF_SMART_BASE).enable();
+        m.addDefaultControls();
+
+        // Grow map to the size of it's parent element
+        const sync = new SMap.Control.Sync();
+        m.addControl(sync);
+
+        // create marker layer we will later fill
+        layer = new SMap.Layer.Marker();
+        m.addLayer(layer);
+        layer.enable();
+
+        console.log('pasting coords into map');
+        {% for p in waypoints %}
+        let card = addPoint({{ p.coord_x }}, {{ p.coord_y }}, "{{ p.name }}", "{{p.description}}");
+        {% endfor %}
+
+    });
+    console.log('calling Loader finished');
+
+});
+
+
+function addPoint(x, y, name, description){
+
+         /* Create card with location miniature */
+        var card = new SMap.Card();
+        card.getHeader().innerText = name;
+        card.getBody().innerText = description;
+
+
+        /* create marker with embedded card */
+        var options = { title: name };
+        var marker = new SMap.Marker(SMap.Coords.fromWGS84(x, y), "Marker" + x + y, { title: name });
+        marker.decorate(SMap.Marker.Feature.Card, card);
+
+        /* add marker to map */
+        layer.addMarker(marker);
+        console.log("Added " + name);
+
+        return card
+
+}
